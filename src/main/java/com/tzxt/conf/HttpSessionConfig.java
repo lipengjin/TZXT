36668f485e8555aa60d00d03c41b845445cd71c8
wangshang <wangshang.a@qq.com>
2017-05-22 06:06:32 +0800

    add session on redis

---
+++ src/main/java/com/tzxt/conf/HttpSessionConfig.java
@@ -0,0 +1,6 @@
+package com.tzxt.conf;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+}
